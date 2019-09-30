bcc9db687e0049f80f96e372454c525eb42f09dd
Kaiqi <lkq137055338@gmail.com>
2019-09-30 22:05:07 +1000

    Pass the first pytest

--- server/channel.py
+++ server/channel.py
@@ -1,2 +1,6 @@
 def channel_invite(token, channel_id, u_id):
     raise ValueError
+
+def channels_create(token, name, is_public):
+    if len(name) > 20:
+        raise ValueError
--- server/channel_test.py
+++ server/channel_test.py
@@ -1,6 +1,11 @@
 import pytest
 from channel import *
 
-def test_invalid:
-    with pytest.raise(ValueError):
-        channel_invite
+def test_invite():
+    with pytest.raises(ValueError):
+        channel_invite('token',0,0)
+        channel_invite('',0,0)
+
+def test_create():
+    with pytest.raises(ValueError):
+        channels_create('token','012345678901234567890',True)
