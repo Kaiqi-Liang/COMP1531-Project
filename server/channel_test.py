5ce8d55e3bab2a99d33030708bb096e3c7c212ec
Kaiqi <lkq137055338@gmail.com>
2019-10-01 13:50:47 +1000

    Complete the stub functions

--- server/channel_test.py
+++ server/channel_test.py
@@ -3,12 +3,16 @@ from channel import *
 
 def test_invite():
     with pytest.raises(ValueError):
+        # invalid token
         channel_invite('token',0,0)
-        channel_invite('',0,0)
+        # empty token
+        channel_invite('token',0,0)
+        # invalid channel id
+        channel_invite('token',0,0)
+        # invalid user id
+        channel_invite('token',0,0)
 
 def test_create():
     with pytest.raises(ValueError):
+        # name is more than 20 characters long
         channels_create('token','012345678901234567890',True)
-
-    auth_register()
-    auth_login()
