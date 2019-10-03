d56fb2c09b043973d609aad4a8fd07fa61c90e6b
Sarah Williams <z5205420@ad.unsw.edu.au>
2019-10-03 14:11:55 +1000

    Update pytests with new information from videos

--- server/message_test.py
+++ server/message_test.py
@@ -1,178 +1,87 @@
 from message import *
 import pytest
 import datetime
+from access_error import AccessError
 
-''' SETUP FOR TESTING '''
-
-''' tokens, type: string'''
-@pytest.fixture
-def token1():
-    return "123"
+# FUNCTION SETUP 
 
 @pytest.fixture
-def token2():
-    return "456"
+def register_owner():
+    authRegisterDict1 = auth_register("sarah@gmail.com", "123", "Sarah", "Williams")
+    # return { u_id, token }
+    return authRegisterDict1
     
 @pytest.fixture
-def token3():
-    return "789"
-
-''' message_id, type: integer '''
-@pytest.fixture
-def messageid1():
-    return 01
-
-@pytest.fixture
-def messageid2():
-    return 02
-    
-''' channel id's, type: integer '''
-@pytest.fixture
-def channelid1():
-    return 11
+def register_user():
+    authRegisterDict2 = auth_register("emma@gmail.com", "456", "Emma", "Mayall")
+    # return { u_id, token }
+    return authRegisterDict2
     
 @pytest.fixture
-def channelid2():
-    return 22
+def create_channel(register_owner):
+    token = register_owner['token']
+    channel = channels_create(token, "Test channel 1", True)
+    # return channel_id
+    return channel
+
+
+@pytest.fixture
+def join_user(register_user):
+    token = register_user['token']
+    channelId = create_channel(register_owner['token'])
+    channel_join(token, channelId)
+    # nothing to return 
+
+@pytest.fixture
+def message_valid():
+    return "Hello World"
     
 @pytest.fixture
-def channelid3():
-    return 33
-    
-''' messages, type: list of dictionaries where each dictionary contains types 
-    { u_id, message, time_created, is_unread } '''
-@pytest.fixture
-def messages1():
-    messagesList = [{'u_id': 100, 'message': 'Hello World', 'time_created': 
-                    datetime(2019, 3, 1, 7, 0, 0, 0), 'is_unread': 0},{'u_id': 2
-                    'message': 'Second message', 'time_created': 
-                    datetime(2019, 10, 8, 7, 7, 0, 0), 'is_unread': 1} ]
-    return messagesList
-
-@pytest.fixture
-def messages2():
-    messagesList = [{'u_id': 5, 'message': 'Good morning!', 'time_created': 
-                    datetime(2019, 8, 10, 8, 0, 0, 0), 'is_unread': 0},{'u_id': 6
-                    'message': 'Hello, how are you?', 'time_created': 
-                    datetime(2019, 8, 10, 9, 7, 0, 0), 'is_unread': 0}, 
-                    {'u_id': 5, 'message': 'Doing well, big day ahead', 'time_created'
-                    : datetime(2019, 8, 10, 9, 30, 0, 0), 'is_unread': 1}]
-    return messagesList
-
-
-''' this message will raise an exception because of its length ''' 
-@pytest.fixture
-def messages3():
-    messagesList = [{'u_id': 30, 'message': 'Cranial exterior: Frontal bone: forms the  forehead, roofs of the orbits. Parietal bones: paired, form the greater portion of the sides and roof of the cranial cavity. Temporal bones: paired, form the lateral aspects of the cranium. Occipital bones:  forms the posterior part and most of the base of the cranium. Sphenoid bone: middle part of the base of the skull. Key part of the cranial floor, holds bones together (butterfly shape). Ethmoid bone: anterior part of the cranial floor, supporting structure of the nasal cavity. Foramen magnum: large hole on the inferior part of the bone. Occipital condyles: oval processes on either side of the foramen magnum, allows you to nod. External acoustic meatus: ear canal which directs sound waves into the ear. Mastoid process: rounded projection of the mastoid portion of the temporal bone (behind the ear). Cranial interior:Anterior cranial fossa: depression in the floor of the cranial base, housing the frontal lobes. Middle cranial fossa: depression in the middle region of the cranial base, and is deeper and wider than the anterior cranial fossa', 'time_created': 
-                    datetime(2019, 10, 8, 8, 0, 0, 0), 'is_unread': 1}]
-                    
-    return messagesList
-
-
-''' time_sent, type: datetime '''
-''' datetime(year, month, day, hour, minute, second, microsecond) '''
-@pytest.fixture
-def time1():
-    dt1 = datetime(2019, 12, 3, 05, 30, 30, 0)
-    return dt1
-   
-''' in the past and so should raise an exception ''' 
-@pytest.fixture
-def time2():
-    dt2 = datetime(2019, 5, 2, 14, 14, 14, 0)
-    return dt2
+def message_invalid():
+    return "Cranial exterior: Frontal bone: forms the  forehead, roofs of the orbits. Parietal bones: paired, form the greater portion of the sides and roof of the cranial cavity. Temporal bones: paired, form the lateral aspects of the cranium. Occipital bones:  forms the posterior part and most of the base of the cranium. Sphenoid bone: middle part of the base of the skull. Key part of the cranial floor, holds bones together (butterfly shape). Ethmoid bone: anterior part of the cranial floor, supporting structure of the nasal cavity. Foramen magnum: large hole on the inferior part of the bone. Occipital condyles: oval processes on either side of the foramen magnum, allows you to nod. External acoustic meatus: ear canal which directs sound waves into the ear. Mastoid process: rounded projection of the mastoid portion of the temporal bone (behind the ear). Cranial interior:Anterior cranial fossa: depression in the floor of the cranial base, housing the frontal lobes. Middle cranial fossa: depression in the middle region of the cranial base, and is deeper and wider than the anterior cranial fossa"    
  
 @pytest.fixture
-def time3():
-    dt3 = datetime(2020, 3, 4, 20, 0, 0, 0)
-    return dt3
+def time_valid():
+    return datetime(2019, 12, 3, 05, 30, 30, 0)
 
-''' in the past and so should raise an exception ''' 
 @pytest.fixture
-def time4():
-    dt4 = datetime(2005, 5, 5, 20, 0, 0, 0)
-    return dt4
-        
-''' react_id, type: integer '''
-@pytest.fixture
-def react1():
-    return 1
+def time_invalid():
+    return (2005, 5, 5, 20, 0, 0, 0) 
+
+
+
+# FUNCTION TESTING 
+''' need to pass in the fixture that I have set up -- this is where i may need
+to make mor as I go long ''' 
+
+# testing for message_sendlater
+#potentially break this up into smaller functions
+def test_message_sendlater(register_owner, create_channel, message_valid, message_invalid, time_valid, time_invalid):
     
-@pytest.fixture
-def react2():
-    return 2
- 
-@pytest.fixture   
-def react3():
-    return 3
-
-'''  ---TESTING FUNCTIONS -- '''
-'''still unsure about how to test a lot of these and whether they can be tested '''
-
-
-''' testing for message_sendlater '''
-'''def message_sendlater(token, channel_id, message, time_sent):'''
-
-def test_laterNormal(): 
-
-''' function raises an exception when the channel doesn't exist '''
-def test_laterNoChannel():
-    with pytest.raises(Exception):
-        message_sendlater(token1, 0, messages1, time1)
-        message_sendlater(token2, 55, messages2, time3)
-        message_sendlater(token2, 3, messages1, time1)
+    # success
+    message_sendlater(register_owner['token'], create_channel, message_valid, time_valid)
+    
+    with pytest.raises(ValueError):
+        # channel doesn't exist -> don't know yet if can test
+        # message > 1000 characters
+        message_sendlater(register_owner['token'], create_channel, message_invalid, time_invalid)
+        # time sent is in the past
+        message_sendlater(register_owner['token'], create_channel, message_valid, time_invalid)
         
-''' function raises an exception when message exceeds 1000 characters '''
-def test_laterExceedes():
-    with pytest.raises(Exception):
-        message_sendlater(token1, channelid1, messages3, time1)
-        message_sendlater(token2, channelid2, messages3, time3)
-        message_sendlater(token3, channelid3, messages3, time3)
-
-''' function raises an eception when scheduled time is in the past ''' 
-def test_laterPast():
-    with pytest.raises(Exception):
-        message_sendlater(token1, channelid1, messages1, time2)
-        message_sendlater(token2, channelid2, messages3, time2)
-        message_sendlater(token3, channelid3, messages3, time4)
-
-''' testing for message_send '''
-'''def message_send(token, channel_id, message):'''
-def test_sendNormal():
-
-''' function raises an exception when message exceeds 1000 characters '''
-def test_sendExceedes():
-    with pytest.raises(Exception):
-        message_send(token1, channelid1, messages3)
-        message_send(token2, channelid2, messages3)
-        message_send(token3, channelid3, messages3)
-
-''' testing for  message_remove'''
-'''def message_remove(token, message_id):'''
-def test_removeNormal():
-
-''' raises an exception when message id does not exist ''' 
-def test_removeNotExist():
-    with pytest.raises(Exception):
-        message_remove(token1, 03)
-        message_remove(token2, 04)
-        message_remove(token3, 05) 
-
-''' raises an exception if user does not have permission to remove row '''
-''' don't know how to test this yet as need to check how permission id works '''  
-def test_removePermission():
-    with pytest.raises(Exception):
         
+def test_message_send(register_owner, create_channel, message_valid, message_invalid)
+    
+    # success
+    # send message as owner
+    message_sendlater(register_owner['token'], create_channel, message_valid)
+    # send message as user 
+    message_sendlater(register_user['token'], create_channel, message_valid)
+    
+    with pytest.raises(ValueError):
+        # message > 1000 characters
+        message_send(register_owner['token'], create_channel, message_invalid)
+    
 
-''' testing for message_edit  '''
-'''def message_edit(token, message_id, message):'''
-def test_editNormal():
-
-''' raises an exception when the user is not the poster of the message '''
-def test_editNotPoster():
-
-def test_editNVUser():
-
-def test_editNVAdmin():
+        
 
 
