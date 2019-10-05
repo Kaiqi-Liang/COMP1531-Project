ec4954df22a7fdef3cb1c04da193d31ae0d8b181
Sarah Williams <z5205420@ad.unsw.edu.au>
2019-10-05 19:08:27 +1000

    Continue working on all tests and fixing style. Nearly finished

--- server/message_test.py
+++ server/message_test.py
@@ -20,7 +20,7 @@ def register_user():
 @pytest.fixture
 def create_channel(register_owner):
     token = register_owner['token']
-    channel = channels_create(token, "Test channel 1", True)
+    channel = channels_create(token, "Test channel", False)
     # return channel_id
     return channel
  
@@ -49,18 +49,18 @@ def time_invalid():
 
 @pytest.fixture
 def messages_list(register_owner, register_user):
-    messages = [{'message_id' = 1, 'u_id' = register_owner['u_id'], 'message' = "Hello", 'time_created' = datetime(2019, 5, 3, 07, 0, 0, 0)}, {'message_id' = 2, 'u_id' = register_user['u_id'], 'message' = "How are you?", 'time_created' = datetime(2019, 5, 3, 07, 30, 0, 0)}, {'message_id' = 3, 'u_id' = register_owner['u_id'], 'message' = "I'm good thanks", 'time_created' = datetime(2019, 5, 3, 8, 0, 0, 0)}]
+    messages = [{'message_id' : 1, 'u_id' : register_owner['u_id'], 'message': "Hello",   'time_created' : datetime(2019, 5, 3, 07, 0, 0, 0), is_unread: False}, 
+    {'message_id' : 2, 'u_id' : register_user['u_id'], 'message' : "How are you?", 'time_created' : datetime(2019, 5, 3, 07, 30, 0, 0), is_unread: False},
+     {'message_id' : 3, 'u_id' : register_owner['u_id'], 'message' : "I'm good thanks", 'time_created' : datetime(2019, 5, 3, 8, 0, 0, 0), is_unread: False}]
     return messages
 
 @pytest.fixture
 def messages_list2(register_owner, register_user):
-    messages2 = [{'message_id' = 4, 'u_id' = register_user['u_id'], 'message' = "Valid Message", 'time_created' = datetime(2019, 7, 4, 07, 0, 0, 0)}, {'message_id' = 5, 'u_id' = register_owner['u_id'], 'message' = "Valid Message", 'time_created' = datetime(2019, 7, 4, 07, 30, 0, 0)}, {'message_id' = 6, 'u_id' = register_user['u_id'], 'message' = "Valid Message", 'time_created' = datetime(2019, 7, 4, 8, 0, 0, 0)}, 'message_id' = 7, 'u_id' = register_user['u_id'], 'message' = "Valid Message", 'time_created' = datetime(2019, 7, 4, 9, 0, 0, 0)}, ]
+    messages2 = [{'message_id' : 4, 'u_id' : register_user['u_id'], 'message' : "Valid Message", 'time_created' : datetime(2019, 7, 4, 07, 0, 0, 0), is_unread: False},
+    {'message_id' : 5, 'u_id' : register_owner['u_id'], {'message' : "Valid Message", 'time_created' : datetime(2019, 7, 4, 07, 30, 0, 0), is_unread: False}, 
+    {'message_id' : 6, 'u_id' : register_user['u_id'], 'message' : "Valid Message", 'time_created' : datetime(2019, 7, 4, 8, 0, 0, 0), is_unread: False}, 
+    {'message_id' : 7, 'u_id' : register_user['u_id'], 'message' : "Valid Message", 'time_created' : datetime(2019, 7, 4, 9, 0, 0, 0, is_unread: False)}]
     return messages2
-
-
-@pytest.fixture
-def message_sent(register_owner, create_channel)
-    message_send(register_owner['token'], create_channel, "Hello")
     
 @pytest.fixture
 def valid_reactId():
@@ -73,96 +73,137 @@ def invalid_reactId():
 
 # FUNCTION TESTING 
 
-
 # testing for message_sendlater
 def test_message_sendlater(register_owner, register_user, create_channel, message_valid, message_invalid, time_valid, time_invalid):
     
-        # success
-    message_sendlater(register_owner['token'], create_channel, message_valid, time_valid)
-    message_sendlater(register_user['token'], create_channel, message_valid, time_valid)
-    
-        
+    # success
+    message_sendlater(register_owner['token'], create_channel, message_valid, time_valid) 
+    assert channel_messages(register_owner['token'], create_channel, 0) == {'messages' = [{'message_id' : 50, 'u_id' : register_owner['u_id'], 'message' : "Hello World", 'time_created' : datetime(2019, 12, 3, 05, 30, 30, 0), is_unread: True}], 'start' = 0, 'end' = -1}
+               
     with pytest.raises(ValueError):
+        # channel based on Id doesn't exist
+        message_sendlater(register_owner['token'], -1, message_valid, time_valid)
+        message_sendlater(register_user['token'], -1, message_valid, time_valid)
         # message > 1000 characters
         message_sendlater(register_owner['token'], create_channel, message_invalid, time_valid)
         message_sendlater(register_user['token'], create_channel, message_invalid, time_valid)
         # time sent is in the past
         message_sendlater(register_owner['token'], create_channel, message_valid, time_invalid)
         message_sendlater(register_user['token'], create_channel, message_valud, time_inalid)
-        
-        
-def test_message_send(register_owner, register_user create_channel, message_valid, message_invalid)
-
-    
+      
+     
+# testing for message_send      
+def test_message_send(register_owner, register_user create_channel, message_valid, message_invalid):
+   
     # success
-    # send message as owner
-    message_sendlater(register_owner['token'], create_channel, message_valid)
-    # send message as user 
-    message_sendlater(register_user['token'], create_channel, message_valid)
+    message_sendlater(register_owner['token'], create_channel, message_valid) 
+    assert channel_messages(register_owner['token'], create_channel, 0) == {'messages' = [{'message_id' : 50, 'u_id' : register_owner['u_id'], 'message' : "Hello World", 'time_created' : datetime.now(), is_unread: False}, 'start' = 0, 'end' = -1}
     
     with pytest.raises(ValueError):
         # message > 1000 characters
         message_send(register_owner['token'], create_channel, message_invalid)
         message_send(register_user['token'], create_channel, message_invalid)
 
-# NOT YET FINISHED     
+# testing for message_remove     
 def test_message_remove(register_owner, register_user, messages_list, messages_list2):
     
     # success 
-    # message sent by the user (logged in person), not the owner
+    # message sent by the user (logged in person), not the owner of channel
     delete_message = messages_list2[0]
     message_remove(register_user['token'], delete_message['message_id'])
+    assert messages_list2 == [ {'message_id' : 5, 'u_id' : register_owner['u_id'], 'message' : "Valid Message", 'time_created' : datetime(2019, 7, 4, 07, 30, 0, 0), is_unread: False}, 
+    {'message_id' : 6, 'u_id' : register_user['u_id'], 'message' : "Valid Message",  'time_created' : datetime(2019, 7, 4, 8, 0, 0, 0), is_unread: False}, 
+    {'message_id' = 7, 'u_id' : register_user['u_id'], 'message' : "Valid Message", 'time_created' : datetime(2019, 7, 4, 9, 0, 0, 0), is_unread: False}]
     
-    # message sent by the user, not an admin
+    # message sent by the user, who is not an admin
     delete_message = messages_list2[1]
     message_remove(register_user['token'], delete_message['message_id'])
+    assert messages_list2 == [{'message_id' : 5, 'u_id' : register_owner['u_id'], 'message' : "Valid Message", 'time_created' : datetime(2019, 7, 4, 07, 30, 0, 0), is_unread: False}, 
+    {'message_id' : 7, 'u_id' : register_user['u_id'], 'message' : "Valid Message", 'time_created' : datetime(2019, 7, 4, 9, 0, 0, 0), is_unread: False} ]
     
-    # message not sent by the user, is an owner 
-    delete_messgae = messages_list[1]
-    message_remove(register_owner['token'], delete_message['message_id'])
-    
-    # message not sent by the user, is an admin
-    valid_admin = 1 # just using this as a placeholder permission_id for 
-                    # a valid amin
-    admin_userpermission_change(register_owner['token'], register_owner['u_id'], valid_admin)
+    # message not sent by the user, who is an owner ofthe channel
     delete_message = messages_list2[1]
+    message_remove(register_owner['token'], delete_message['message_id'])
+    assert messages_list2 == [{'message_id' : 5, 'u_id' : register_owner['u_id'], 'message' : "Valid Message", 'time_created' : datetime(2019, 7, 4, 07, 30, 0, 0), is_unread: False}]
+    
+    # message not sent by the user, who is an admin
+    admin_userpermission_change(register_owner['token'], register_owner['u_id'], 2)
+    delete_message = messages_list[1]
     message_remove(register_owner['token'], delete_message['message_id']
-        
+    assert messages_list == [{'message_id' : 1, 'u_id' : register_owner['u_id'], 'message' : "Hello", 'time_created' : datetime(2019, 5, 3, 07, 0, 0, 0), is_unread: False}, 
+    {'message_id' : 3, 'u_id' : register_owner['u_id'], 'message' : "I'm good thanks", 'time_created' : datetime(2019, 5, 3, 8, 0, 0, 0), is_unread: False}]
+ 
+    delete_message = messages_list[0]
+
     with pytest.raises(ValueError):
         # message based on id does not exist
         # this should raise an error as message has already been removed above
-        delete_message = messages_list1[1]
-        message_remove(register_owner['token'], delete_message['message_id'])    
+        message_remove(register_owner['token'], 2) 
+           
     with pytest.raises(AccessError):
         # message was sent by logged in user, message was sent by owner / admin
-        delete_message = messages_list[0]
         message_remove(register_owner['token'], delete_message['message_id')
-       
-# NEED TO FINISH 
-def test_message_edit(messages_list, register_owner )
-
-def test_message_react(register_user, register_owner, messages_list, messages_list2 valid_reactId, invalid_reactId):
+ 
+# testing for message_edit      
+def test_message_edit(messages_list, register_owner, register_user):
+    
+    # success
+    # message sent by user (logged in person), not an owner or admin
+    message = messages_list[1]
+    message_edit(register_user['token'], message['message_id'], "Edit own")
+    
+    assert messages_list == [{'message_id' : 1, 'u_id' : register_owner['u_id'], 'message' : "Hello", 'time_created' : datetime(2019, 5, 3, 07, 0, 0, 0), is_unread: False}, 
+    {'message_id' : 2, 'u_id' : register_user['u_id'], 'message' : "Edit own", 'time_created' : datetime(2019, 5, 3, 07, 30, 0, 0), is_unread: False}, 
+    {'message_id' : 3, 'u_id' : register_owner['u_id'], 'message' : "I'm good thanks", 'time_created' : datetime(2019, 5, 3, 8, 0, 0, 0), is_unread: False}]
+    
+    
+    # message not sent by user, is an owner
+    message = messages_list[1]
+    message_edit(register_owner['token'], message['message_id'], "Edit others")
+    
+    assert messages_list == [{'message_id' : 1, 'u_id' : register_owner['u_id'], 'message' : "Hello", 'time_created' : datetime(2019, 5, 3, 07, 0, 0, 0), is_unread: False}, 
+    {'message_id' : 2, 'u_id' : register_user['u_id'], 'message' : "Edit others", 'time_created' : datetime(2019, 5, 3, 07, 30, 0, 0), is_unread: False}, 
+    {'message_id' : 3, 'u_id' : register_owner['u_id'], 'message' : "I'm good thanks", 'time_created' : datetime(2019, 5, 3, 8, 0, 0, 0), is_unread: False}]
+    
+    # message not sent by the user, is an admin
+    message = messages_list[1]
+    admin_userpermission_change(register_owner['token'], register_owner['u_id'], 2)
+    message_edit(register_owner['token'], message['message_id'], "Edit by admin")
+    
+    assert messages_list == [{'message_id' : 1, 'u_id' : register_owner['u_id'], 'message' : "Hello", 'time_created' : datetime(2019, 5, 3, 07, 0, 0, 0), is_unread: False}, 
+    {'message_id' : 2, 'u_id' : register_user['u_id'], 'message' : "Edit by admin", 'time_created' : datetime(2019, 5, 3, 07, 30, 0, 0), is_unread: False}, 
+    {'message_id' : 3, 'u_id' : register_owner['u_id'], 'message' : "I'm good thanks", 'time_created' : datetime(2019, 5, 3, 8, 0, 0, 0), is_unread: False}]
+    
+    with pytest.raises(ValueError):
+        # message sent by user and user is an owner/admin
+        message = messages_list[0]
+        message_edit(register_owner['token'], message['message_id'], "Not allowed Edit")
+    
+# testing for message_react
+def test_message_react(register_user, register_owner, messages_list, valid_reactId, invalid_reactId):
 
     # success
     message = messages_list[0]
     message_react(register_user['token'], message['message_id'], valid_reactId)
     message = messages_list[1]
-    message_react(register_ower['token'], message['message_id'], valid_reactId)
+    message_react(register_owner['token'], message['message_id'], valid_reactId)
     
-    with pytest,raises(ValueError);
+    message1 = messages_list[2]
+    message2 = messages_list[0]
+    
+    with pytest.raises(ValueError):
+        # message_id is not a valid message
+        message_unpin(register_owner['token'], -1, valid_reactId)
         # react id is not valid
-        message = messages_list[2]
-        message_react(register_user['token'], message['message_id'], invalid_reactId)
-        message = messages_list2[0]
-        message_react(register_owner['token'], message['message_id'], invalid_reactId)
+        message_react(register_user['token'], message1['message_id'], invalid_reactId)
+        message_react(register_owner['token'], message1['message_id'], invalid_reactId)
         # message already has a react 
         # as the above tests have reacted to a message, use this messages to now
         # raise an exception
-        message = messages_list[0]
-        message_react(register_user['token'], message['message_id'], valid_reactId)
-        message_react(register_owner['token'], message['message_id'], valid_reactId)
+        message_react(register_user['token'], message2['message_id'], valid_reactId)
+        message_react(register_owner['token'], message2['message_id'], valid_reactId)
     
-    
+# testing for message_unreact    
 def test_message_unreact(register_user, messages_list, valid_reactId, invalid_reactId):
 
     # success
@@ -171,51 +212,57 @@ def test_message_unreact(register_user, messages_list, valid_reactId, invalid_re
     message = messages_list[1]
     message_unreact(register_ower['token'], message['message_id'], valid_reactId)
     
-    with pytest.raises(ValueError);
+    message = messages_list[2]
+    message1 = messages_list[0]
+    
+    with pytest.raises(ValueError):
+        # message_id is not a valid message
+        message_unpin(register_owner['token'], -1, valid_reactId)
         # react id is not valid
-        message = messages_list[2]
-        message_unreact(register_user['token'], message'message_id'], invalid_reactId)
-        message = messages_list2[0]
+        message_unreact(register_user['token'], message['message_id'], invalid_reactId)
         message_unreact(register_owner['token'], message['message_id'], invalid_reactId)
         # message doesn't have a react
-        # the above tests have removed a react from messages so use these 
-        # messages to raise the error
-        message = messages_list[0]
-        message_unreact(register_user['token'], message['message_id'], valid_reactId)
-        message_unreact(register_owner['token'], message['message_id'], valid_reactId
+        # the above tests have removed a react from messages so use these messages 
+        message_unreact(register_user['token'], message1['message_id'], valid_reactId)
+        message_unreact(register_owner['token'], message1['message_id'], valid_reactId
 
+# testing for message_pin
 def test_message_pin(register_user, register_owner, messages_list):
 
-   # success
-   message = messages_list[0]
-   valid_admin = 1 # just using this as a placeholder permission_id for 
-                    # a valid amin
-    admin_userpermission_change(register_owner['token'], register_owner['u_id'], valid_admin)
-   message_pin(register_owner['token'], message['message_id']
+    # success
+    message = messages_list[0]
+    admin_userpermission_change(register_owner['token'], register_owner['u_id'], 2)
+    message_pin(register_owner['token'], message['message_id'])
    
-   with pytest.raises(ValueError):
+    message = messages_list[1]
+    message1 = messages_list[0]
+    with pytest.raises(ValueError):
+        # message_id is not a valid message
+        message_unpin(register_owner['token'], -1)
         # authorised user is not an admin
-        message = messages_list[1]
         message_pin(register_user['token'], message['message_id'])
         # message is already pinned
-        message = messages_list[0]
-        message_pin(register_owner['token'], message['message_id'])
+        message_pin(register_owner['token'], message1['message_id'])
         
-        
+# testing for message_unpin      
 def test_message_unpin(register_user, register_owner, messages_list):       
         
- # success
+    # success
     message = messages_list[0]
-    valid_admin = 1 # just using this as a placeholder permission_id for 
-                    # a valid amin
-    admin_userpermission_change(register_owner['token'], register_owner['u_id'], valid_admin)
-    message_unpin(register_owner['token'], message['message_id']
+    admin_userpermission_change(register_owner['token'], register_owner['u_id'], 2)
+    message_unpin(register_owner['token'], message['message_id'])
+    
+    message = messages_list[1]
+    message1 = messages_list[0]
    
-   with pytest.raises(ValueError):
-       # authorised user is not an admin
-       message = messages_list[1]
-       message_unpin(register_user['token'], message['message_id'])
-       # message is already unpinned
-       message = messages_list[0]
-       message_unpin(register_owner['token'], message['message_id'])
+    with pytest.raises(ValueError):
+        # message_id is not a valid message
+        message_unpin(register_owner['token'], -1)
+        # authorised user is not an admin
+        message_unpin(register_user['token'], message['message_id'])
+        # message is already unpinned
+        message_unpin(register_owner['token'], message1['message_id'])
+        
+    # should also be raising an access error, have addressed this in assumptions 
+   
 
