77428001a245fe907fb3cc084cac34f6c7094923
andybui01 <andy.bui2001@gmail.com>
2019-10-11 09:35:13 +1100

    shutup

--- server/tests/test_channel.py
+++ server/tests/test_channel.py
@@ -1,3 +1,10 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+import config
+
 # pip3 INSTALLS
 import pytest
 
