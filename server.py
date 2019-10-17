9cca62e3e3be991265b0c08034e58526286459eb
Kaiqi <lkq137055338@gmail.com>
2019-10-18 09:05:44 +1100

    Fix style

--- server.py
+++ server.py
@@ -5,12 +5,14 @@ from flask import Flask, request
 from backend.auth import *
 
 DATA = {
-    'user': [], #[{'u_id': u_id1, 'email': email1, 'password': password1}, {'u_id': u_id2, 'email': email2, 'password': password2}]
+    'user': [],
+#   [{'u_id': u_id1, 'email': email1, 'password': password1}, {'u_id': u_id2, 'email': email2, 'password': password2}]
     'channel': [],
     'message': []
 }
 
 def get_data():
+    ''' get global variable ie. database'''
     global DATA
     return DATA
 
