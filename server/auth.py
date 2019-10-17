59b773e69e0ddcd17f32b42c5c1b50c581064d73
andybui01 <andy.bui2001@gmail.com>
2019-10-17 11:07:51 +1100

    push to remote

--- server/auth.py
+++ server/auth.py
@@ -4,6 +4,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
+from app import get_data
 
 def auth_login (email,password):
     user = server.get_data()['user']
