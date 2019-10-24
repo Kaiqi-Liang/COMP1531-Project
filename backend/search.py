72ef59865bf36c4791f86f12b6ad821b14f3ca31
emma <emmarosemayall@gmail.com>
2019-10-24 12:03:59 +1100

    started search function

--- backend/search.py
+++ backend/search.py
@@ -8,7 +8,14 @@ sys.path.insert(0,parentdir)
 from server import get_data   # server.py
 
 def search(token, query_st):
+    #Given a query string, return a collection of messages in all of the channels
+    #that the user has joined that match the query
+    users = channels['members']
+    messages = channels['messages']
+    for users in query_st:
+        messages_match = messages.find(raw_input(""))
+
     if query_st == "":
         raise ValueError("Invalid search")
-    messages = {}
-    return messages
+
+    return messages_match
