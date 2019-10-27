8f1ffdf6021e03c1771ca05664ee98014a194af5
Sarah Williams <z5205420@ad.unsw.edu.au>
2019-10-27 18:08:46 +1100

    Finished tests

--- tests/admin_test.py
+++ tests/admin_test.py
@@ -4,26 +4,29 @@ import pytest
 ''' Local packages '''
 from backend.auth import auth_register
 from backend.auth import auth_login
-from admin import admin_userpermission_change
+from backend.admin import admin_userpermission_change
+from backend.database import clear
+from backend.helpers.exception import ValueError, AccessError
 
 def test_admin_userpermission_change1():
     # setup begin
+    clear()
     loginDict = auth_register("emmarosemayall@gmail.com", "123456", "Emma", "Mayall")
     token = loginDict["token"]
     uid = loginDict["u_id"]
     #setup end
 
     #test for when we have a valid permission id
-    admin_userpermission_change(token, uid, 1)
-    pass
+    admin_userpermission_change(token, uid, 2)
 
 def test_admin_userpermission_change2():
     # setup begin
-    loginDict = auth_login("emmarosemayall@gmail.com", "123456")
+    clear()
+    loginDict = auth_register("emmarosemayall@gmail.com", "123456", "Emma", "Mayall")
     token = loginDict["token"]
     uid = loginDict["u_id"]
     #setup end
 
     #test for when we have an invalid permission id
     with pytest.raises(ValueError, match=r"*"):
-        admin_userpermission_change (token, uid, 5)
+        admin_userpermission_change(token, uid, 5)
