9ca92357fe7cd0e0df2e94b8d5db72607991cb58
Sarah Williams <z5205420@ad.unsw.edu.au>
2019-10-27 17:52:37 +1100

    fixing pytests

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -5,178 +5,268 @@ import pytest
 from backend.channel import *
 from backend.auth import *
 from backend.database import clear
+from backend.helpers.exception import AccessError, ValueError
 
 @pytest.fixture
 def register_owner():
     # return { u_id, token }
-    return auth_register('z5210932@unsw.edu.au', '123456', 'Kaiqi', 'Liang')
-
+    owner_dict = auth_register('z5210932@unsw.edu.au', '123456', 'Kaiqi', 'Liang')
+    return owner_dict
+    
 @pytest.fixture
 def register_user():
     # return { u_id, token }
     return auth_register('kaiqi.liang9989@gmail.com', '123456', 'kaiqi', 'liang')
 
 @pytest.fixture
-def channel_create(register_owner):
+def channel_create(owner_token):
     # return channel_id
-    return channels_create(register_owner['token'], 'name', True)['channel_id']
+    return channels_create(owner_token, 'name', True)['channel_id']
 
-
-def test_invite(register_owner, channel_create, register_user):
+# working
+def test_invite():
     clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_dict = register_user()
+    
     with pytest.raises(ValueError):
         # invalid channel id
-        channel_invite(register_owner['token'], -1, register_user['u_id'])
+        channel_invite(owner_token, -1, user_dict['u_id'])
         # invalid user id
-        channel_invite(register_owner['token'], channel_create, 'user_id')
+        channel_invite(owner_token, channel_id, 'user_id')
 
     # invite user to the channel
-    channel_invite(register_owner['token'], channel_create, register_user['u_id'])
-    assert channel_detail(register_user['token'], channel_create) == { 'name', [{'u_id': register_own['u_id'], 'name_first': 'Kaiqi', 'name_last': 'Liang'}], [{'u_id': register_own['u_id'], 'name_first': 'Kaiqi', 'name_last': 'Liang'}, {'u_id': register_user['u_id'], 'name_first': 'kaiqi', 'name_last': 'liang'}] }
+    channel_invite(owner_token, channel_id, user_dict['u_id'])
+    assert channel_details(user_dict['token'], channel_id) == {'name': 'name', 'owner_members': [{'u_id': owner_user, 'name_first': 'Kaiqi', 'name_last': 'Liang'}], 'all_members' : [{'u_id': owner_user, 'name_first': 'Kaiqi', 'name_last': 'Liang'}, {'u_id': user_dict['u_id'], 'name_first': 'kaiqi', 'name_last': 'liang'}]}
 
-
-def test_details(register_owner, channel_create, register_user):
+def test_details():
     clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_dict = register_user()
     # check if the owner is in the channel after the channel is first created
-    assert channel_detail(register_owner['token'], channel_create) == { 'name', [{'u_id': register_own['u_id'], 'name_first': 'Kaiqi', 'name_last': 'Liang'}], [{'u_id': register_own['u_id'], 'name_first': 'Kaiqi', 'name_last': 'Liang'}] }
+    assert channel_details(owner_token, channel_id) == {'name':'name', 'owner_members': [{'u_id': owner_user, 'name_first': 'Kaiqi', 'name_last': 'Liang'}], 'all_members': [{'u_id': owner_user, 'name_first': 'Kaiqi', 'name_last': 'Liang'}]}
 
     with pytest.raises(ValueError):
         # channel does not exist
-        channel_detail(register_owner['token'], -1)
-
-    with pytest.raises(AccessError):
-        # user is not a member of channel
-        channel_detail(register_user['token'], channel_create)
-
-
-def test_message(register_owner, channel_create, register_user):
+        channel_details(owner_token, -1)
+# not working        
+def test_details1():
     clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_dict = register_user()
+    with pytest.raises(AccessError, match=r"*"):
+        # user is not a member of channel
+        channel_details(user_dict['token'], channel_id)
+
+
+def test_message():
+    clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_dict = register_user()
     # no messages in the channel at the moment
-    assert channel_message(register_owner['token'], channel_create, 0) == { 'messages': [], 'start': 0, 'end': 0 }
+    assert channel_messages(owner_token, channel_id, 0) == {'messages': [], 'start': 0, 'end': -1}
     with pytest.raises(ValueError):
         # channel does not exist
-        channel_message(register_owner['token'], -1, 0)
+        channel_messages(owner_token, -1, 0)
         # start is greater than the total number of messages
-        channel_message(register_owner['token'], channel_create, 1)
-
+        channel_messages(owner_token, channel_id, 1)
+        
+# not working
+def test_message1():
+    clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_dict = register_user()
     with pytest.raises(AccessError):
         # user is not a member of channel
-        channel_message(register_user['token'], channel_create, 0)
+        channel_messages(user_dict['token'], channel_id, 0)
 
 
-def test_leave(register_owner, channel_create):
+def test_leave():
     clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_dict = register_user()
     with pytest.raises(ValueError):
         # channel does not exist
-        channel_leave(register_owner['token'], -1)
+        channel_leave(owner_token, -1)
 
     # owner leaves the channel
-    channel_leave(register_owner['token'], channel_create)
+    channel_leave(owner_token, channel_id)
 
     with pytest.raises(ValueError):
         # channel does not exist anymore after the owner leaves the channel
-        channel_leave(register_owner['token'], channel_create)
+        channel_leave(owner_token, channel_id)
 
 
-def test_join(register_owner, channel_create, register_user):
+def test_join():
     clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_dict = register_user()
     # user joins a public channel created by the owner
-    channel_join(register_user['token'], channel_create)
-    assert channel_detail(register_user['token'], channel_create) == { 'name', [{'u_id': register_own['u_id'], 'name_first': 'Kaiqi', 'name_last': 'Liang'}], [{'u_id': register_own['u_id'], 'name_first': 'Kaiqi', 'name_last': 'Liang'}, {'u_id': register_user['u_id'], 'name_first': 'kaiqi', 'name_last': 'liang'}] }
+    channel_join(user_dict['token'], channel_id)
+    assert channel_details(user_dict['token'], channel_id) == {'name': 'name', 'owner_members': [{'u_id': owner_user, 'name_first': 'Kaiqi', 'name_last': 'Liang'}], 'all_members':[{'u_id': owner_user, 'name_first': 'Kaiqi', 'name_last': 'Liang'}, {'u_id': user_dict['u_id'], 'name_first': 'kaiqi', 'name_last': 'liang'}]}
 
     with pytest.raises(ValueError):
         # channel does not exist
-        channel_join(register_owner['token'], -1)
-
-    # owner creates a private channel for user to join
-    channel_id = channels_create(register_owner['token'], 'private', False)['channel_id']
-    with pytest.raises(AccessError):
-        channel_join(register_user['token'], channel_id)
-
-
-def test_addowner(register_owner, channel_create, register_user):
+        channel_join(owner_token, -1)
+# not working
+def test_join1():
     clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_dict = register_user()
+    # owner creates a private channel for user to join
+    channel_id = channels_create(owner_token, 'private', False)['channel_id']
+    with pytest.raises(AccessError):
+        channel_join(user_dict['token'], channel_id)
+
+
+def test_addowner():
+    clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_dict = register_user()
     # make user the owner of the channel
-    channel_invite(register_owner['token'], channel_create, register_user['u_id'])
-    channel_addowner(register_owner['token'], channel_create, register_user['u_id'])
-    assert channel_detail(register_user['token'], channel_create) == { 'name', [{'u_id': register_own['u_id'], 'name_first': 'Kaiqi', 'name_last': 'Liang'}, {'u_id': register_user['u_id'], 'name_first': 'kaiqi', 'name_last': 'liang'}], [{'u_id': register_own['u_id'], 'name_first': 'Kaiqi', 'name_last': 'Liang'}, {'u_id': register_user['u_id'], 'name_first': 'kaiqi', 'name_last': 'liang'}] }
+    channel_invite(owner_token, channel_id, user_dict['u_id'])
+    channel_addowner(owner_token, channel_id, user_dict['u_id'])  
+    assert channel_details(user_dict['token'], channel_id) == {'name': 'name', 'owner_members': [{'u_id': owner_user, 'name_first': 'Kaiqi', 'name_last': 'Liang'}, {'u_id': user_dict['u_id'], 'name_first': 'kaiqi', 'name_last': 'liang'}], 'all_members':[{'u_id': owner_user, 'name_first': 'Kaiqi', 'name_last': 'Liang'}, {'u_id': user_dict['u_id'], 'name_first': 'kaiqi', 'name_last': 'liang'}]}
 
     with pytest.raises(ValueError):
         # channel does not exist
-        channel_addowner(register_owner['token'], -1, register_user['u_id'])
+        channel_addowner(owner_token, -1, user_dict['u_id'])
         # user is already an owner
-        channel_addowner(register_owner['token'], channel_create, register_user['u_id'])
+        channel_addowner(rowner_token, channel_id, user_dict['u_id'])
 
+# not working      
+def test_addowner1():
+    clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_dict = register_user()
+    channel_invite(owner_token, channel_id, user_dict['u_id'])
+    channel_addowner(owner_token, channel_id, user_dict['u_id']) 
     # remove user as an owner
-    channel_removeowner(register_owner['token'], -1, register_user['u_id'])
+    channel_removeowner(owner_token, -1, user_dict['u_id'])
     with pytest.raises(AccessError):
         # user has no permission to add owner
-        channel_addowner(register_user['token'], channel_create, register_owner['u_id'])
+        channel_addowner(user_dict['token'], channel_id, owner_user)
 
 
-def test_removeowner(register_owner, channel_create, register_user):
+def test_removeowner():
     clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_dict = register_user()
     # make user the owner of the channel then remove it
-    channel_invite(register_owner['token'], channel_create, register_user['u_id'])
-    channel_addowner(register_owner['token'], channel_create, register_user['u_id'])
-    channel_removeowner(register_owner['token'], channel_create, register_user['u_id'])
-    assert channel_detail(register_user['token'], channel_create) == { 'name', [{'u_id': register_own['u_id'], 'name_first': 'Kaiqi', 'name_last': 'Liang'}], [{'u_id': register_own['u_id'], 'name_first': 'Kaiqi', 'name_last': 'Liang'}, {'u_id': register_user['u_id'], 'name_first': 'kaiqi', 'name_last': 'liang'}] }
+    channel_invite(owner_token, channel_id, user_dict['u_id'])
+    channel_addowner(owner_token, channel_id, user_dict['u_id'])
+    channel_removeowner(owner_token, channel_id, user_dict['u_id'])
+    assert channel_details(user_dict['token'], channel_id) == { 'name':'name','owner_members': [{'u_id': owner_user, 'name_first': 'Kaiqi', 'name_last': 'Liang'}], 'all_members' : [{'u_id': owner_user, 'name_first': 'Kaiqi', 'name_last': 'Liang'}, {'u_id': user_dict['u_id'], 'name_first': 'kaiqi', 'name_last': 'liang'}] }
 
     with pytest.raises(ValueError):
         # channel does not exist
-        channel_removeowner(register_owner['token'], -1, register_user['u_id'])
+        channel_removeowner(owner_token, -1, user_dict['u_id'])
         # user is not an owner
-        channel_removeowner(register_user['token'], channel_create, register_owner['u_id'])
-
-    channel_removeowner(register_owner['token'], -1, register_user['u_id'])
+        channel_removeowner(user_dict['token'], channel_id, owner_user)
+        
+# not working    
+def test_removeowner1():
+    clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_dict = register_user()
+    channel_removeowner(owner_token, -1, user_dict['u_id'])
     with pytest.raises(AccessError):
         # not an owner
-        channel_removeowner(register_user['token'], channel_create, register_owner['u_id'])
+        channel_removeowner(user_dict['token'], channel_id, owner_user)
 
 
-def test_list(register_owner, channel_create, register_user):
+def test_list():
     clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+   # channel_id = channel_create(owner_token)
+    user_dict = register_user()
     # user is not part of any channels
-    assert channels_list(register_user['token']) == []
+    assert channels_list(owner_user) == []
     # owner can see the channel created
-    assert channels_list(register_owner['token']) == [{'id': channel_create, 'name': 'name'}]
+    assert channels_list(owner_token) == [{'id': channel_id, 'name': 'name'}]
 
     # user creates a private channel
-    channel_id = channels_create(register_user['token'], 'private', False)['channel_id']
+    channel_id = channels_create(user_dict['token'], 'private', False)['channel_id']
     # user can see this private channel
-    assert channels_list(register_user['token']) == [{'id': channel_id, 'name': 'private'}]
+    assert channels_list(user_dict['token']) == [{'id': channel_id, 'name': 'private'}]
 
     # owner leaves the channel
-    channel_leave(register_owner['token'], channel_create)
+    channel_leave(owner_token, channel_id)
     # owner can not see any channels
-    assert channels_listall(register_owner['token']) == []
+    assert channels_listall(owner_token) == []
 
 
-def test_listall(register_owner, register_user):
+def test_listall():
     clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    #channel_id = channel_create(owner_token)
+    user_dict = register_user()
     # there is no channels at this point
-    assert channels_listall(register_user['token']) == []
-    assert channels_listall(register_onwer['token']) == []
+    assert channels_listall(user_dict['token']) == []
+    assert channels_listall(owner_token) == []
 
     # owner creates a public channel
-    channel_id = channels_create(register_owner['token'], 'public', True)['channel_id']
+    channel_id = channels_create(owner_token, 'public', True)['channel_id']
     # user can see this public channel
-    assert channels_listall(register_user['token']) == [{'id': channel_id, 'name': 'public'}]
+    assert channels_listall(user_dict['token']) == [{'id': channel_id, 'name': 'public'}]
 
     # owner creates a private channel
-    channel_id = channels_create(register_owner['token'], 'private', False)['channel_id']
+    channel_id = channels_create(owner_token, 'private', False)['channel_id']
     # owner can see this private channel
-    assert channels_listall(register_owner['token']) == [{'id': channel_id, 'name': 'private'}]
+    assert channels_listall(owner_token) == [{'id': channel_id, 'name': 'private'}]
     # user can not see the private channel
-    assert channels_listall(register_user['token']) == []
+    assert channels_listall(user_dict['token']) == []
 
 
-def test_create(register_owner):
+def test_create():
     clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
     # owner creates a channel
-    channel_id = channels_create(register_owner['token'], 'name', True)
+    channel_id = channels_create(owner_token, 'name', True)['channel_id']
     # a chanel has been created
-    assert channels_list(register_owner['token']) == [{'id': channel_create, 'name': 'name'}]
+    assert channels_list(owner_token) == [{'id': channel_id, 'name': 'name'}]
 
     with pytest.raises(ValueError):
         # name is more than 20 characters long
