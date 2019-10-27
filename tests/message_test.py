6b600c8af30e3f2c19a134d6c72539ce82f4ec83
Sarah Williams <z5205420@ad.unsw.edu.au>
2019-10-27 15:23:04 +1100

    Finish tests

--- tests/message_test.py
+++ tests/message_test.py
@@ -40,12 +40,6 @@ def create_channel(owner_token):
     return channels_create(owner_token, "Test channel", True)
 
 @pytest.fixture
-def join_user(register_owner, register_user, create_channel):
-    token = register_user['token']
-    channel_join(register_owner['token'], create_channel, register_user['u_id'])
-    # nothing to return
-
-@pytest.fixture
 def message_valid():
     return "Hello World"
 
@@ -53,13 +47,6 @@ def message_valid():
 def message_invalid():
     return "Cranial exterior: Frontal bone: forms the  forehead, roofs of the orbits. Parietal bones: paired, form the greater portion of the sides and roof of the cranial cavity. Temporal bones: paired, form the lateral aspects of the cranium. Occipital bones:  forms the posterior part and most of the base of the cranium. Sphenoid bone: middle part of the base of the skull. Key part of the cranial floor, holds bones together (butterfly shape). Ethmoid bone: anterior part of the cranial floor, supporting structure of the nasal cavity. Foramen magnum: large hole on the inferior part of the bone. Occipital condyles: oval processes on either side of the foramen magnum, allows you to nod. External acoustic meatus: ear canal which directs sound waves into the ear. Mastoid process: rounded projection of the mastoid portion of the temporal bone (behind the ear). Cranial interior:Anterior cranial fossa: depression in the floor of the cranial base, housing the frontal lobes. Middle cranial fossa: depression in the middle region of the cranial base, and is deeper and wider than the anterior cranial fossa"
 
-@pytest.fixture
-def time_valid():
-    return datetime(2019, 12, 3, 5, 30, 30, 0)
-
-@pytest.fixture
-def time_invalid():
-    return (2005, 5, 5, 20, 0, 0, 0)
 
 @pytest.fixture
 def valid_reactid():
