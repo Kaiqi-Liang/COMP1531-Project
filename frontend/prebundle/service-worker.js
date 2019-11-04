fbb83c4124dad56a6fee45404b6e609eff5981b5
Kaiqi <kaiqi.liang9989@gmail.com>
2019-11-04 18:12:20 +1100

    Change frontend

--- frontend/prebundle/service-worker.js
+++ frontend/prebundle/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed827df55d116a1fd411dfbc28625a03.js"
+  "/precache-manifest.905577613dcaf9f0fc5549cfa1ce6d05.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
