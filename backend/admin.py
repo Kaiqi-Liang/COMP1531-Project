5ef6d00e5e693bbd8526e928e9d46442d8093d8e
Kaiqi Liang <z5210932@ad.unsw.edu.au>
2019-11-14 20:30:48 +1100

    Change admin back

--- backend/admin.py
+++ backend/admin.py
@@ -1,55 +1,67 @@
-from backend.database import get_data, get_channel, get_user
+""" Admin functions """
+from backend.database import get_data, get_user
 from backend.helpers.token import get_user_from_token
 from backend.helpers.exception import ValueError, AccessError
 
 def admin_userpermission_change(token, u_id, permission_id):
     slackr = get_data()["slackr"]
-    u_id = int(u_id)
+    try:
+        u_id = int(u_id)
+    except:
+        raise ValueError("u_id does not refer to a valid user")
     permission_id = int(permission_id)
 
     user1 = get_user(get_user_from_token(token))
     user2 = get_user(u_id)
-    if permission_id > 3:
+    if permission_id > 3 or permission_id < 1:
         raise ValueError("permission_id not valid")
-    if permission_id < -1:
-        raise ValueError("permission_id not valid")
-    if user2 == None:
+    if user2 is None:
         raise ValueError("u_id does not refer to a valid user")
 
     # if user2's permission is the same as permission_id
     if user2['permission_id'] == permission_id:
-        raise AccessError("Trying to change to same permission!")
+        return {}
 
     # if user1 is the owner of the slackr
     if user1["permission_id"] == 1:
         # user1 cannot remove themselves if they are the only owner ...
         if len(slackr['admin']) == 1 and user2 == user1:
-            raise AccessError("You are the only owner!")
+            return {}
         # since user1 can change permissions of everyone ...
         try:
             slackr['owner'].remove(user2['u_id'])
         except:
-            ValueError
+            raise ValueError
         try:
             slackr['admin'].remove(user2['u_id'])
         except:
-            ValueError
+            raise ValueError
 
         user2["permission_id"] = permission_id
+        if user2["permission_id"] <= 2:
+            slackr['admin'].append(user2['u_id'])
+            if user2["permission_id"] == 1:
+                slackr['owner'].append(user2['u_id'])
+
     # if user1 is an admin of the slackr
     elif user1["permission_id"] == 2:
         # if user2 is an owner of the slackr, user1 cannot change
         if user2["permission_id"] == 1 or permission_id == 1:
-            raise AccessError("You're not an owner, you can't do that!")
-        else:
-            try:
-                slackr['admin'].remove(user2['u_id'])
-            except:
-                ValueError
+            raise AccessError("The authorised user is not an owner")
 
-            user2["permission_id"] = permission_id
-    # members don't have rights >:)
+        try:
+            slackr['admin'].remove(user2['u_id'])
+        except:
+            raise ValueError
+
+        user2["permission_id"] = permission_id
+        if user2["permission_id"] <= 3:
+            slackr['member'].append(user2['u_id'])
+            if user2["permission_id"] == 2:
+                slackr['admin'].append(user2['u_id'])
+
+    # members don't have rights
     else:
-        raise AccessError("Members don't have rights!")
-    print("\t\t\treached end")
+        raise AccessError("The authorised user is not an admin or owner")
+
     return {}
