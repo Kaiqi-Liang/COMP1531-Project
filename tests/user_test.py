051e873c5c0410511b0a4bf1934d927bbd79cca8
Sarah Williams <z5205420@ad.unsw.edu.au>
2019-11-16 11:31:14 +1100

    Add in extra test cases. Coverage on user.py now at 77%

--- tests/user_test.py
+++ tests/user_test.py
@@ -8,7 +8,24 @@ from PIL import Image
 from backend.user import *
 from backend.auth import auth_register
 from backend.database import clear
+from backend.channel import channels_create, channel_details
 from backend.helpers.exception import ValueError, AccessError
+from backend.helpers.token import generate_token
+
+@pytest.fixture
+def register_owner():
+    # return { u_id, token }
+    return auth_register('z5210932@unsw.edu.au', '123456', 'Sarah', 'Williams')
+
+@pytest.fixture
+def channel_create(owner_token):
+    # return channel_id
+    return channels_create(owner_token, 'name', 'true')['channel_id']
+
+@pytest.fixture
+def register_user():
+    # return { u_id, token }
+    return auth_register('kaiqi.liang9989@gmail.com', '123456', 'kaiqi', 'liang')
 
 def test_user_all():
     clear()
@@ -21,7 +38,7 @@ def test_user_profile1():
 	ret = auth_register("someemail@gmail.com","securepassword","John","Doe")
 	user_profile(ret['token'],ret['u_id'])
 
-	assert user_profile(ret['token'], ret['u_id'])=={'email':"someemail@gmail.com",'name_first':"John",'name_last':"Doe",'handle_str':"johndoe", 'profile_img_url': None, 'u_id' : ret['u_id']}
+	assert user_profile(ret['token'], ret['u_id'])== {'email':"someemail@gmail.com",'name_first':"John",'name_last':"Doe",'handle_str':"johndoe", 'profile_img_url': None, 'u_id' : ret['u_id']}
 
 def test_user_profile2():
 	clear()
@@ -32,6 +49,12 @@ def test_user_profile2():
 	invalid_u_id = 2
 	with pytest.raises(ValueError, match=r"*"):
 		user_profile(ret['token'], invalid_u_id)
+
+# user with token doesn't exist
+def test_user_profile3():
+    clear()
+    owner_dict = register_owner()
+    assert user_profile(generate_token(100), owner_dict['u_id']) == {}
 
 def test_user_profile_setname1():
 	clear()
@@ -66,6 +89,17 @@ def test_user_profile_setname4():
 		# Both names exceeding 50 chars ...
 		user_profile_setname(ret['token'], "hugefirstnamethatiscertainlylongerthanfiftycharacters", "hugesecondtnamethatiscertainlylongerthanfiftycharacters")
 
+# check that the name has changed in the channel members and owners lists 
+def test_user_profile_setname5():
+    clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    owner_user = owner_dict['u_id']
+    channel_id = channel_create(owner_token)
+    user_profile_setname(owner_token, "sarah", "williams")
+    
+    assert channel_details(owner_token, channel_id) == {'name': 'name', 'owner_members': [{'u_id': owner_user, 'name_first': 'sarah', 'name_last': 'williams', 'profile_img_url': None}], 'all_members' : [{'u_id': owner_user, 'name_first': 'sarah', 'name_last': 'williams', 'profile_img_url': None}]}
+
 def test_user_profile_setemail1():
 	clear()
 	ret = auth_register("someemail1@gmail.com","securepassword","John","Doe")
@@ -90,7 +124,6 @@ def test_user_profile_setemail3():
 	ret = auth_register("someemail1@gmail.com","securepassword","John","Doe")
 
 	with pytest.raises(ValueError, match=r"*"):
-
 		# This should be invalid because the email address is bonkers ...
 		user_profile_setemail(ret['token'], "notvalidemail.com")
 
@@ -111,19 +144,20 @@ def test_user_profile_sethandle2():
 
 	# Invalid case ...
 	# Assume handles can be a max 20 characters and not minimum ...
-	with pytest.raises(ValueError, match=r"*"):
+	with pytest.raises(ValueError):
 		# This shouldn't work because the handle is too long ...
 		user_profile_sethandle(ret['token'], "johndoe12345678910111213")
 
 def test_user_profile_sethandle3():
-	clear()
-	ret = auth_register("someemail1@gmail.com","securepassword","John","Doe")
-
 	# Test that the same handle cannot be used again
-	user_profile_sethandle(ret['token'], "johndoe")
-	user = get_user(get_user_from_token(ret['token']))
-
-	assert user['handle_str'] != "johndoe1"
+    clear()
+    owner_dict = register_owner()
+    owner_token = owner_dict['token']
+    user_dict = register_user()
+    
+    user_profile_sethandle(owner_token, "BestCoder")
+    with pytest.raises(ValueError):
+        user_profile_sethandle(user_dict['token'], "BestCoder")
 
 def test_user_profile_uploadphoto():
     clear()
@@ -153,3 +187,14 @@ def test_user_profile_uploadphoto():
 
         # Valid crop but invalid url ...
         user_profiles_uploadphoto(user['token'], invalid_url, 0, 0, 800, 800, base_url)
+
+# user w token doesn't exist
+def test_user_profile_uploadphoto2():
+    clear()
+    img_url = "https://yt3.ggpht.com/a/AGF-l7_fK0Hy4B4JO8ST-uGqSU69OTLHduk4Kri_fQ=s900-c-k-c0xffffffff-no-rj-mo"
+    invalid_url = "https://google.com/404"
+    base_url = "localhost:5000"
+    png_url = "https://www.fnordware.com/superpng/pnggrad16rgb.png"
+    assert user_profiles_uploadphoto(generate_token(100), img_url, 0, 0, 800, 800, base_url) == {}
+          
+
