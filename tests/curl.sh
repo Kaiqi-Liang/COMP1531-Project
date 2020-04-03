651bd6efd23c8e4fc54ecab0ca9745a098b25a95
Kaiqi <kaiqi.liang@unsw.edu.au>
2020-04-03 10:19:57 +0800

    Add comments about curl

--- tests/curl.sh
+++ tests/curl.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+# curl parameters
+# -s --silent: Don't show progress meter or error messages, but still output the data or even stdout
+# -S --show-error: When use with -s it makes curl show an error message if it fails
+# -X --request <command>: CRUD or default GET when not specified
+# -d --data <data>
+# -H --header <header/@file>: -H "Content-Type: application/json"
+
 token=`curl -s -X post -d "name_first=eric&name_last=pull&email=123@123.com&password=123456" localhost:5001/auth/register`
 token=`echo $token | cut -d\" -f6`
 channel_id=`curl -s -X post -d "token=$token&name=channel&is_public=true" localhost:5001/channels/create`
